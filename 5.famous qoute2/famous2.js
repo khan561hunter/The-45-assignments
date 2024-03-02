a8f56186085f9fcf67b69c685b130fa8f79220a9
Wania Khan <160246913+khan561hunter@users.noreply.github.com>
2024-03-02 22:34:07 +0500

    Delete 5.famous qoute2 directory

--- 5.famous qoute2/famous2.js
+++
@@ -1,3 +0,0 @@
-var famous_person = "Martin king Jr";
-var message = "i have a dream";
-console.log("".concat(famous_person, " once said,\"").concat(message, "\""));
--- 5.famous qoute2/famous2.ts
+++
@@ -1,3 +0,0 @@
-let famous_person:string="Martin king Jr";
-let message:string="i have a dream";
-console.log(`${famous_person} once said,"${message}"`);
