1bf2a68966959863f4c08e4a21228729a7721930
Wania Khan <160246913+khan561hunter@users.noreply.github.com>
2024-03-02 22:27:47 +0500

    Delete 2.personal message directory

--- 2.personal message/personal.js
+++
@@ -1,8 +0,0 @@
-var person_name = '';
-person_name = prompt("What is your name?") || '';
-if (person_name !== null && person_name !== '') {
-    alert("Hello ".concat(person_name, ",would you like to learn some Python today?"));
-}
-else {
-    alert("you need to fill out smth ");
-}
--- 2.personal message/personal.ts
+++
@@ -1,9 +0,0 @@
-let person_name:string='';
-person_name=prompt("What is your name?") || '';
-if (person_name!== null && person_name!=='')
-{
-    alert(`Hello ${person_name},would you like to learn some Python today?`);
-}
-else{
-    alert(`you need to fill out smth `);
-}
