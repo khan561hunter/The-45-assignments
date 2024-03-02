91352eb0c258c6ad376bcfc460817a798b450039
Wania Khan <160246913+khan561hunter@users.noreply.github.com>
2024-03-02 09:55:55 -0800

    Add files via upload

---
+++ 19.list of arrays/list.js
@@ -0,0 +1,4 @@
+var list1 = ['urdu', 'english', 'russian'];
+var list2 = [];
+list2.push('urdu', 'english', 'russian');
+console.log(list2);
---
+++ 19.list of arrays/list.ts
@@ -0,0 +1,5 @@
+
+let list1 : string[]=['urdu','english','russian'];
+let list2 : string [] = [];
+list2.push('urdu','english','russian');
+console.log(list2);
