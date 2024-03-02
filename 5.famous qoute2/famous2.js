9a25e9fe50b89584e386b6ec28fbe613f4d41fd1
Wania Khan <160246913+khan561hunter@users.noreply.github.com>
2024-03-02 09:14:34 -0800

    Add files via upload

---
+++ 5.famous qoute2/famous2.js
@@ -0,0 +1,3 @@
+var famous_person = "Martin king Jr";
+var message = "i have a dream";
+console.log("".concat(famous_person, " once said,\"").concat(message, "\""));
---
+++ 5.famous qoute2/famous2.ts
@@ -0,0 +1,3 @@
+let famous_person:string="Martin king Jr";
+let message:string="i have a dream";
+console.log(`${famous_person} once said,"${message}"`);
