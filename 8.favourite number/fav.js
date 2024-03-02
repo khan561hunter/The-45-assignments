2edf3ecf9e5a4912c5e8e3e37dc52b5484058ec2
Wania Khan <160246913+khan561hunter@users.noreply.github.com>
2024-03-02 22:37:44 +0500

    Delete 8.favourite number directory

--- 8.favourite number/fav.js
+++
@@ -1,3 +0,0 @@
-var fav = 5;
-var message = 'here is your favourite number :' + fav;
-console.log(message);
--- 8.favourite number/fav.ts
+++
@@ -1,3 +0,0 @@
-const fav:number=5;
-const message:string='here is your favourite number :'+fav;
-console.log(message);
