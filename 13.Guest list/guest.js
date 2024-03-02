74994eb04939bfd419c32cfdf8891088bec712fc
Wania Khan <160246913+khan561hunter@users.noreply.github.com>
2024-03-02 09:41:08 -0800

    Add files via upload

---
+++ 13.Guest list/guest.js
@@ -0,0 +1,4 @@
+var guest = ['Imran khan', 'babar Azam', 'naseem shah'];
+for (var i = 0; i < guest.length; i = i + 1) {
+    console.log("Dear Mr.".concat(guest[i], "\n                Its a pleasure to invite you today\n                thanks for joining us!"));
+}
---
+++ 13.Guest list/guest.ts
@@ -0,0 +1,7 @@
+let guest :string[]=['Imran khan','babar Azam','naseem shah'];
+for (let i=0;i<guest.length;i=i+1)
+{
+    console.log(`Dear Mr.${guest[i]}
+                Its a pleasure to invite you today
+                thanks for joining us!`);
+}
