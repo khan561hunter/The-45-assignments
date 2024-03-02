7b5c4063ac5a7ca95bc1562bd111573dd46aab55
Wania Khan <160246913+khan561hunter@users.noreply.github.com>
2024-03-02 09:21:26 -0800

    Add files via upload

---
+++ 11.greetings/greet.js
@@ -0,0 +1,5 @@
+var person = ['sana', 'krooz', 'hania'];
+var message = "hey! there";
+for (var i = 0; i < person.length; i++) {
+    console.log("".concat(person[i], ":").concat(message));
+}
---
+++ 11.greetings/greet.ts
@@ -0,0 +1,6 @@
+let person: string[]=['sana','krooz','hania'];
+let message : string="hey! there";
+for (let i=0;i<person.length;i++)
+{
+    console.log(`${person[i]}:${message}`);
+}
