e1dfc6dd1baf9ece2c7769c1dc2f74f5eed2e3ea
Wania Khan <160246913+khan561hunter@users.noreply.github.com>
2024-03-02 09:40:18 -0800

    Add files via upload

---
+++ 12.Your own array/your.js
@@ -0,0 +1,4 @@
+var vehicle = ['motorcycle', 'cycle', 'bike', 'car'];
+for (var i = 0; i < vehicle.length; i++) {
+    console.log("\"i would like to own a ".concat(vehicle[i], "\""));
+}
---
+++ 12.Your own array/your.ts
@@ -0,0 +1,5 @@
+let vehicle:string[]=['motorcycle','cycle','bike','car'];
+for (let i=0 ; i<vehicle.length ;i++)
+{
+    console.log(`"i would like to own a ${vehicle[i]}"`);
+}
