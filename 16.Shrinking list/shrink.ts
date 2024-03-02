bce9fa48477c453994b1f957baa1665b411a47e8
Wania Khan <160246913+khan561hunter@users.noreply.github.com>
2024-03-02 09:44:19 -0800

    Add files via upload

---
+++ 16.Shrinking list/shrink.ts
@@ -0,0 +1,34 @@
+let guest :string[]=['Imran khan','babar Azam','naseem shah']; 
+
+for (let i=0;i<guest.length;i=i+1)
+{
+    console.log('Dear Mr.'+ guest[i] + ',\n\nIts a pleasure to invite you today thanks for joining us!\n\n');
+}
+console.log(`we have a bigger table to adjust more 3 people`);
+guest.unshift('Hamza');
+guest.push('Ahmed');
+guest.splice(2,0,'Bilal');
+for (let i=0;i<guest.length;i=i+1)
+{
+    console.log('Dear Mr.'+ guest[i] + ',\n\nIts a pleasure to invite you today thanks for joining us!\n\n');
+}
+
+console.log(`Sorry ! we only have a table for two people only`);
+
+
+while( guest.length > 2)
+{
+    let removed_guest = guest.pop();
+console.log(`Sorry Mr ${removed_guest} cant invite you to dinner`);
+}
+
+for(let i=0 ; i<guest.length ; i=i+1)
+{
+    console.log('Dear Mr.'+ guest[i] + ',\n\nIts a pleasure to invite you today thanks for joining us!\n\n');
+}
+
+guest.splice(0,2);
+console.log(guest);
+
+
+
